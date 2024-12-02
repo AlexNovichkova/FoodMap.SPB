3425e71e772ff38141c4e82015c8361402a85773
kikitooo <nik.puchok@gmail.com>
2024-12-02 20:25:41 +0300

    #backend added get methods for restaurants and cuisines

--- services/backend/FOODMAP/cuisines/views.py
+++ services/backend/FOODMAP/cuisines/views.py
@@ -1,8 +1,16 @@
 from rest_framework import viewsets
 from .serializers import CuisineSerializer
 from .models import Cuisine
-
+from rest_framework.response import Response
+from rest_framework import status
 
 class CuisineView(viewsets.ModelViewSet):
     serializer_class = CuisineSerializer
     queryset = Cuisine.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        data = {
+                    "success": True,
+                    "cuisines": self.queryset
+                }
+        return Response(data, status=status.HTTP_200_OK)
