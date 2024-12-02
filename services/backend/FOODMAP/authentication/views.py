b562d22b17ff6a403aa4026d0a893513b4c76a61
kikitooo <nik.puchok@gmail.com>
2024-12-02 18:08:15 +0300

    #backend + frontend added patch method

--- services/backend/FOODMAP/authentication/views.py
+++ services/backend/FOODMAP/authentication/views.py
@@ -76,7 +76,18 @@ class UserAPIView(RetrieveUpdateAPIView):
         user = self.get_object()
         serializer = self.get_serializer(user)
         data = {
-            "success": True,  # Успешный запрос
-            "user": serializer.data  # Данные о пользователе
+            "success": True,
+            "user": serializer.data,
         }
         return Response(data, status=status.HTTP_200_OK)
+    
+    def patch(self, request, *args, **kwargs):
+        user = self.get_object()
+        serializer = self.get_serializer(user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            data = {
+                "success": True,
+                "user": serializer.data
+            }
+            return Response(data, status=status.HTTP_200_OK)
